69aa67ab544ddaf68742a41647c15aa200907fcf
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-09-28 12:27:41 +0700

    commit

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
 import LottieView from 'lottie-react-native';
 
 const OrderScreen = () => {
   return (
-    <View>
-      <LottieView source={require('../assets')} />
-    </View>
+    <SafeAreaView>
+      <LottieView
+        source={require('../assets/thumbs.json')}
+        style={{
+          height: 360,
+          width: 400,
+          alignSelf: 'center',
+          marginTop: 40,
+          justifyContent: 'center',
+        }}
+        autoPlay
+        loop={false}
+        speed={0.8}
+      />
+      <Text
+        style={{
+          marginTop: 40,
+          fontSize: 19,
+          fontWeight: '600',
+          textAlign: 'center',
+        }}
+      >
+        Your order has been placed
+      </Text>
+      <LottieView
+        source={require('../assets/sparkle.json')}
+        style={{
+          height: 300,
+          position: 'absolute',
+          top: 100,
+          width: 300,
+          alignSelf: 'center',
+        }}
+        autoPlay
+        loop={false}
+        speed={0.8}
+      />
+    </SafeAreaView>
   );
 };
 
